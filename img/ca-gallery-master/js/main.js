3abc769e1959df5fde29ce5f98d45966473ea2cc
idan <idanelbaz@gmail.com>
2019-05-28 17:59:32 +0300

    profolio edition 1

---
+++ img/ca-gallery-master/js/main.js
@@ -0,0 +1,96 @@
+console.log('Starting up');
+
+
+$(document).ready(init);
+
+
+function init() {
+    createProjects();
+    renderProj();
+
+}
+
+
+function renderProj() {
+    var strHtml = '';
+    var projects = gprojects;
+
+    for (var i = 0; i < projects.length; i++) {
+        strHtml += ` <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" onclick ="renderModal(${projects[i].id}) " >
+            <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                    <i class="fa fa-plus fa-3x"></i>
+                </div>
+            </div>
+            <img class="img-fluid" src="${projects[i].img}" alt="${projects[i].name}">
+        </a>
+        <div class="portfolio-caption">
+            <h4>${projects[i].name}</h4>
+            <p class="text-muted">${projects[i].desc}</p>
+        </div>
+    </div>`;
+    };
+
+
+    $('.innerPro').html(strHtml);
+
+}
+
+function renderModal(projId) {
+    console.log(projId)
+    var strHtml = '';
+    var projects = gprojects;
+    var proj = projects[projId - 1];
+    console.log(proj)
+
+    strHtml += `
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl"></div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <div class="modal-body">
+                            <!-- Project Details Go Here -->
+                            <h2>${proj.name}</h2>
+        <p class="item-intro text-muted">${proj.title}</p>
+        <img class="img-fluid d-block mx-auto" src="${proj.img}" alt="">
+        <p>${proj.desc}</p>
+      
+        <ul class="list-inline">
+            <li>${proj.publishedAt}</li>
+            <li>${proj.labels}</li>
+            <li>Category: Games</li>
+            <a href="${proj.url}">Visit ${proj.name} </a>
+        </ul>
+        <button class="btn btn-primary" data-dismiss="modal" type="button">
+    <i class="fa fa-times"></i>
+    Close Project</button> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+
+
+    $('.portfolio-modal').html(strHtml);
+    $('.portfolio-modal').modal('show');
+}
+
+function sendMail() {
+    console.log('hi')
+    var email = $('.email').val();
+    var subject = $('.subject').val();
+    var msg = $('.message').val();
+
+    var emailLink = createEmailLink(email, subject, msg);
+    window.open(emailLink)
+}
---
+++ img/ca-gallery-master/js/service.js
@@ -0,0 +1,53 @@
+'use strict'
+
+var gprojects;
+
+
+function createProjects() {
+    var projects = [createProj(1, 'Mine Sweeper', 'Mine Sweeper', 'The best game EVER', 'https://idanelbaz.github.io/mine-sweeper', '16.5.19', 'ELBAZ inc', 'img/wallpapper.jpg'),
+        createProj(2, 'Book Store', 'Book Store', 'very good book store managment tool', 'https://idanelbaz.github.io/book-store/', '27.5.19', 'ELBAZ inc', 'img/new.jpg'),
+        createProj(3, 'Guess Who', 'Guess Who', 'The user need to guess a person', 'https://idanelbaz.github.io/Guess-Who/', '26.5.19', 'ELBAZ inc', 'img/guess.png')
+    ];
+
+
+    gprojects = projects;
+}
+
+
+function createProj(id, name, title, desc, url, publishedAt, labels, img) {
+    return {
+        id: id,
+        name: name,
+        title: title,
+        desc: desc,
+        url: url,
+        publishedAt: publishedAt,
+        labels: labels,
+        img: img,
+
+    }
+}
+
+
+function createEmailLink(email, subject, msg) {
+
+    var strLink = '';
+    strLink += 'https://mail.google.com/mail/?view=cm&fs=1&to=' + 'idanelbaz@gmail.com' + '&su=' + subject + '&body=' + msg;
+
+    return strLink;
+}
+
+
+
+function getTime() {
+    var str = '';
+    var time = new Date();
+    str += time.getDate() + '.';
+    str += Number(time.getUTCMonth()) + 1 + '.';
+    str += time.getFullYear() + ' , ';
+    str += time.getHours() + ":";
+    str += time.getMinutes();
+
+    return str;
+
+}
